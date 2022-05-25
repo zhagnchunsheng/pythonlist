a7c90dd97a5cd576b3d2499c5f6173e947b176e9
zhangcs <455708821@qq.com>
2022-05-25 21:05:33 +0800

    5.25

--- webapp/www/app1.py
+++ webapp/www/app1.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+'''
+async web application.
+'''
+
 import logging; logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
@@ -6,11 +15,10 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome<h1>', content_type='text/html')
+    return web.Response(body=b'<h1>Awesome</h1>',headers={'content-type':'text/html'})
 
-#@asyncio.coroutine
 async def init(loop):
-    app = web.Application()
+    app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
